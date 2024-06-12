d6e9c591356b4d7dd4e060b1ba3275db1f60e745
denizdag14 <dnxdag@gmail.com>
2024-06-12 14:14:27 +0300

    add movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,8 +16,8 @@ export default function Card({result}) {
                 <h2 className="text-lg font-bold truncate">{result.title || result.name}</h2>
                 <p className="line-clamp-2 text-md">{result.overview}</p>
                 <p className="flex items-center ">
-                        {result.release_date || result.first_air_date}
-                        <FiThumbsUp className="h-5 mr-1 ml-3"/> {result.vote_count}
+                    {result.release_date || result.first_air_date}
+                    <FiThumbsUp className="h-5 mr-1 ml-3"/> {result.vote_count}
                 </p>
             </div>
         </Link>
