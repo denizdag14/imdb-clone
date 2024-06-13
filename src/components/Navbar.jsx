a992952eea0aa2eac95cb1abbe207d5751a492e1
denizdag14 <dnxdag@gmail.com>
2024-06-13 14:54:16 +0300

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+'use client';
+
+import { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
   return (
     <div className="flex dark:text-white text-black dark:bg-gray-600 bg-yellow-200 p-4 lg:text-lg justify-center gap-6">
-          <NavbarItem title="Trending" param="fetchTrending"/>
-          <NavbarItem title="Top Rated Movies" param="fetchTopRatedMovie" />
-          <NavbarItem title="Top Rated Tv Series" param="fetchTopRatedTvSeries" />
+          <Suspense fallback={<div>Loading...</div>}>
+            <NavbarItem title="Trending" param="fetchTrending"/>
+            <NavbarItem title="Top Rated Movies" param="fetchTopRatedMovie" />
+            <NavbarItem title="Top Rated Tv Series" param="fetchTopRatedTvSeries" />
+          </Suspense>
     </div>
   )
 }
