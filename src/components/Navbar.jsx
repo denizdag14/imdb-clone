0d11044324390bd54f1876a924dd6755e08af31a
denizdag14 <dnxdag@gmail.com>
2024-06-17 00:14:15 +0300

    navbar improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,101 @@
 'use client';
 
 import { Suspense } from "react";
-import NavbarItem from "./NavbarItem";
+import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react'
 
 export default function Navbar() {
   return (
     <div className="flex dark:text-white text-black dark:bg-gray-600 bg-yellow-200 p-4 lg:text-lg justify-center gap-6">
           <Suspense fallback={<div>Loading...</div>}>
-            <div className={`relative group flex justify-center`}>
-              <p className={`hover:text-amber-600 font-light sm:font-semibold text-xs sm:text-lg `}>Trending</p>
-              <div className={`hidden group-hover:flex flex-col absolute top-full bg-yellow-200 dark:bg-gray-600 rounded-b-md shadow-lg w-32 border-amber-600 border-b border-l border-r`}>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Movies & Tv" param="fetchTrending" />
-                </div>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Person" param="fetchTrendingPerson" />
-                </div>
-              </div>
-            </div>
-            <div className={`relative group flex justify-center`}>
-              <p className={`hover:text-amber-600 font-light sm:font-semibold text-xs sm:text-lg `}>Top Rated</p>
-              <div className={`hidden group-hover:flex flex-col absolute top-full bg-yellow-200 dark:bg-gray-600 rounded-b-md shadow-lg w-28 border-amber-600 border-b border-l border-r`}>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Movies" param="fetchTopRatedMovie" />
-                </div>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Tv Series" param="fetchTopRatedTvSeries" />
-                </div>
-              </div>
-            </div>
-            <div className={`relative group flex justify-center`}>
-              <p className={`hover:text-amber-600 font-light sm:font-semibold text-xs sm:text-lg`}>Upcoming</p>
-              <div className={`hidden group-hover:flex flex-col absolute top-full bg-yellow-200 dark:bg-gray-600 rounded-b-md shadow-lg w-28 border-amber-600 border-b border-l border-r`}>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Movies" param="fetchUpcomingMovie" />
-                </div>
-                <div className="hover:bg-yellow-300 dark:hover:bg-gray-500 p-2 text-center">
-                  <NavbarItem title="Tv Series" param="fetchUpcomingTvSeries" />
-                </div>
-              </div>
-            </div>
+            <Popover>
+              <PopoverButton>
+                Trending
+              </PopoverButton>
+              <Transition
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <PopoverPanel
+                  anchor="bottom"
+                  className="rounded-xl dark:bg-slate-600 bg-yellow-200 text-sm"
+                >
+                  <div className="p-3">
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchTrending">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">Movies & Tv Series</p>
+                      <p className="text-gray-400 dark:text-gray-200">Trending movies and tv series</p>
+                    </a>
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchTrendingPerson">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">People</p>
+                      <p className="text-gray-400 dark:text-gray-200">Trending people</p>
+                    </a>
+                  </div>
+                </PopoverPanel>
+              </Transition>
+            </Popover>
+            
+            <Popover>
+              <PopoverButton>
+                Top Rated
+              </PopoverButton>
+              <Transition
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <PopoverPanel
+                  anchor="bottom"
+                  className="rounded-xl dark:bg-slate-600 bg-yellow-200 text-sm"
+                >
+                  <div className="p-3">
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchTopRatedMovie">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">Movies</p>
+                      <p className="text-gray-400 dark:text-gray-200">Top rated movies</p>
+                    </a>
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchTopRatedTvSeries">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">Tv Series & Shows</p>
+                      <p className="text-gray-400 dark:text-gray-200">Top rated tv series and shows</p>
+                    </a>
+                  </div>
+                </PopoverPanel>
+              </Transition>
+            </Popover>
+
+            <Popover>
+              <PopoverButton>
+                Upcoming
+              </PopoverButton>
+              <Transition
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <PopoverPanel
+                  anchor="bottom"
+                  className="rounded-xl dark:bg-slate-600 bg-yellow-200 text-sm"
+                >
+                  <div className="p-3">
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchUpcomingMovie">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">Movies</p>
+                      <p className="text-gray-400 dark:text-gray-200">Upcoming movies</p>
+                    </a>
+                    <a className="block rounded-lg py-2 px-3 transition dark:hover:bg-slate-700 hover:bg-yellow-400" href="/?genre=fetchUpcomingTvSeries">
+                      <p className="font-semibold dark:text-yellow-500 text-gray-800">Tv Series & Shows</p>
+                      <p className="text-gray-400 dark:text-gray-200">Upcoming tv series and shows</p>
+                    </a>
+                  </div>
+                </PopoverPanel>
+              </Transition>
+            </Popover>
           </Suspense>
     </div>
   )
