3e8a8f48de068659edb07b3e4f6d2db678c797b7
denizdag14 <dnxdag@gmail.com>
2024-06-17 20:19:34 +0300

    add different darkmode switch button

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,6 +2,7 @@
 
 import {useState} from 'react';
 import {useRouter} from 'next/navigation';
+import { Button } from '@headlessui/react'
 
 export default function SearchBox() {
     const [search, setSearch] = useState('');
@@ -12,12 +13,12 @@ export default function SearchBox() {
       setSearch("");
     };
   return (
-    <form className='flex justify-between px-5 max-w-4xl mx-auto pt-4' onSubmit={handleSubmit}>
+    <div className='flex justify-between px-5 max-w-4xl mx-auto pt-4'>
         <input type="text" placeholder="Search keyword..." 
         className="w-full h-11 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1" 
         value={search} 
         onChange={(e) => setSearch(e.target.value)}/>
-        <button className="bg-yellow-400 rounded-md py-1 px-2 text-black dark:disabled:bg-gray-400 h-11 disabled:bg-yellow-200" disabled={search === ""} >Search</button>
-    </form>
+        <Button onClick={handleSubmit} className="bg-yellow-400 rounded-2xl py-1 px-2 text-black dark:disabled:bg-gray-400 h-11 disabled:bg-yellow-200 inline-flex items-center gap-2 text-sm/6 font-semibold shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-yellow-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white" disabled={search === ""} >Search</Button>
+    </div>
   )
 }
